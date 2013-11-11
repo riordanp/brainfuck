f65b8468b96bb2365ebc46084811232847348829
Riordan Panayides <riordan@riordan.eu>
2013-11-11 02:14:11 +0000

    Added interpretation of files via arguments

--- README.md
+++ README.md
@@ -2,7 +2,4 @@ brainfuck
 =======
 
 A brainfuck interpreter written in C++, now with Linux support!
-
-todo
--------
-+ Add reading from a file or passing arguments
+usage: ./brainfuck -u <file path>
--- main.cpp
+++ main.cpp
@@ -1,110 +1,115 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 #include "stdio.h"
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-	string program;
-	//test
-	//brainfuck hello world from wikipedia
-	program = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
-	//prints fibonacci sequence (http://www.hevanet.com/cristofd/brainfuck/fib.b)
-//	program = ">++++++++++>+>+[[+++++[>++++++++<-]>.<++++++[>--------<-]+<<<]>.>>[[-]<[>+<-]>>[<<+>+>-]<[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>[-]>+>+<<<-[>+<-]]]]]]]]]]]+>>>]<<<]";
+	//cout << argv[0] << " " << argv[1] << " " << argv[2] << " " <<  argc << endl;
 	
-	int memory[30000] = {0};
-	int *ptr = memory;
-	int depth;
-
-	for(unsigned int i = 0; i < program.size(); i++)
+	if((argv[1] == "-p") || (argc < 3))
 	{
-		switch(program[i])
+		cout << "[Error] Please select a brainfuck file to run. E.g. ./brainfuck -p test.bf" << endl;
+	}
+	else
+	{
+		ifstream ifile(argv[2]);
+		if(!ifile.is_open())
 		{
-			case '>':
-				if(*ptr++ >= 30000)
+			cout << "[Error] Invalid input file specified" << endl;
+		}
+		else
+		{
+			string program((istreambuf_iterator<char>(ifile)),(istreambuf_iterator<char>())) ;
+	
+			int memory[30000] = {0};
+			int *ptr = memory;
+			int depth;
+
+			for(unsigned int i = 0; i < program.size(); i++)
+			{
+				switch(program[i])
 				{
-					*ptr = 0;
-				}
-			break;
+					case '>':
+						if(*ptr++ >= 30000)
+						{
+							*ptr = 0;
+						}
+					break;
+					case '<':
+						if(*ptr-- < 0)
+						{
+							*ptr = 30000;
+						}
+					break;
+					case '+':
+						(*ptr)++;
+					break;
+					case '-':
+						(*ptr)--;
+					break;
+					case '.':
+						putchar(*ptr);
+					break;
+					case ',':
+						*ptr = getchar();
+					break;
+					case '[':
+						if(*ptr != 0)
+						{
+							break;
+						}
 
-			case '<':
-				if(*ptr-- < 0)
-				{
-					*ptr = 30000;
-				}
-			break;
-
-			case '+':
-				(*ptr)++;
-			break;
-
-			case '-':
-				(*ptr)--;
-			break;
-
-			case '.':
-				putchar(*ptr);
-			break;
-
-			case ',':
-				*ptr = getchar();
-			break;
-
-			case '[':
-				if(*ptr != 0)
-				{
+						depth = 1;
+						while(depth)
+						{
+							i++;
+							if(i >= program.size())
+							{
+								return -1;
+							}
+							if(program[i] == '[')
+							{
+								depth++;
+							}
+							if(program[i] == ']')
+							{
+								depth--;
+							}
+						}
+				
+					break;
+					case ']':
+						if(*ptr == 0)
+						{
+							break;
+						}
+						depth = 1;
+						while(depth)
+						{
+							i--;
+							if(i < 0)
+							{
+								return -1;
+							}
+							if(program[i] == ']')
+							{
+								depth++;
+							}
+							if(program[i] == '[')
+							{
+								depth--;
+							}
+						}
+					
+					break;
+					default:
 					break;
 				}
-
-				depth = 1;
-				while(depth)
-				{
-					i++;
-					if(i >= program.size())
-					{
-						return -1;
-					}
-					if(program[i] == '[')
-					{
-						depth++;
-					}
-					if(program[i] == ']')
-					{
-						depth--;
-					}
-				}
-				
-			break;
-
-			case ']':
-				if(*ptr == 0)
-				{
-					break;
-				}
-				depth = 1;
-				while(depth)
-				{
-					i--;
-					if(i < 0)
-					{
-						return -1;
-					}
-					if(program[i] == ']')
-					{
-						depth++;
-					}
-					if(program[i] == '[')
-					{
-						depth--;
-					}
-				}
-				
-			break;
-
-			default:
-			break;
+			}
+			getchar();
 		}
 	}
-	getchar();
 }
